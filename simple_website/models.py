af25e7f432e04d707f9ef1fac2ece7885b095cbc
r1337331 <rad.siry@gmail.com>
2021-01-20 20:10:15 +0100

    Update

--- simple_website/models.py
+++ simple_website/models.py
@@ -4,7 +4,7 @@ from django_resized import ResizedImageField
 
 class MakePost(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField(max_length=300, blank=True)
+    content = models.TextField(max_length=500, blank=True)
     photo = ResizedImageField(size=[500, 300], quality=100, upload_to='simple_website/images/', null=True, blank=True, default='')
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
